f59a343c23e3113c107a95a4a8582dc4e9af77ae
Laura-navarrete21 <jhulianavarrete@gmail.com>
2025-10-31 18:00:15 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.wrapper', {
+  loop: true,
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
